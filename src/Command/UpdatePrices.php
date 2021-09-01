db092e4d1704444d02b75a9ededa03a386762d45
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-09-01 14:42:46 +0200

    working: update prices of currencies from JSON response from https://api.coincap.io/v2/rates

--- src/Command/UpdatePrices.php
+++ src/Command/UpdatePrices.php
@@ -20,5 +20,39 @@ class UpdatePrices
     {
         // TODO
         // use $this->cryptocurrencyManager->updatePrice() method
+
+        $http_response=file_get_contents('https://api.coincap.io/v2/rates');
+        $json=json_decode($http_response);
+        if($json===null){
+            printf("ERROR in JSON! (possible network error)\n");
+            return;
+        }
+
+        $items=[];
+        foreach($json->data as $item) $items[$item->id]=$item->rateUsd;
+     
+        $currencies=[
+        'bitcoin',
+        'bitcoin-cash',
+        //'cardano', //missing
+        'dash',
+        'ethereum',
+        //'ethereum-classic', //missing
+        'litecoin',
+        //'polkadot', //missing
+        //'stellar', //missing
+        //'tron', //missing
+        ];
+
+        foreach($currencies as $id){
+            $price=@$items[$id];
+            if($price===null){
+                printf("%s: not found!\n",$id);
+                continue;
+            }
+            echo $id," ",$price,"\n";
+            $this->cryptocurrencyManager->updatePrice($id,$price);
+        }
+        printf("Update done.\n");
     }
 }
--- src/Model/CryptocurrencyManager.php
+++ src/Model/CryptocurrencyManager.php
@@ -46,5 +46,10 @@ class CryptocurrencyManager
     public function updatePrice(string $id, float $price): void
     {
         // TODO
+
+        $query = $this->database->prepare('UPDATE cryptocurrencies SET price=:price WHERE id=:id');
+        $query->bindParam(':price', $price);
+        $query->bindParam(':id', $id);
+        $query->execute();
     }
 }
